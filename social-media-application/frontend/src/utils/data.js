c4525ea5aadcf24b80362dca541f22e008def8fd
Purya Behdarvandan <purya.B97@gmail.com>
2022-11-20 23:10:06 +0300

    Feed

--- social-media-application/frontend/src/utils/data.js
+++ social-media-application/frontend/src/utils/data.js
@@ -2,3 +2,53 @@ export const userQuery = (userId) => {
   const query = `*[_type == "user" && _id == '${userId}']`;
   return query;
 };
+
+export const searchQuery = (serachTerm) => {
+  const query = `*[_type == "pin" && title match '${serachTerm}*' || category match '${serachTerm}*' || about match '${serachTerm}*']{
+    image {
+      assets -> {
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy -> {
+      _id,
+      userName,
+      image
+    },
+    save[] {
+      _key,
+      postedBy -> {
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+
+  return query;
+};
+
+export const feedQuery = `*[_type == 'pin'] | order(_createAt desc) {
+  image {
+    assets -> {
+      url
+    }
+  },
+  _id,
+  destination,
+  postedBy -> {
+    _id,
+    userName,
+    image
+  },
+  save[] {
+    _key,
+    postedBy -> {
+      _id,
+      userName,
+      image
+    },
+  },
+}`;
