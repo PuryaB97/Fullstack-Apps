b046ca00a508d20fcfab668134362c92af12601e
Purya Behdarvandan <purya.B97@gmail.com>
2022-11-21 23:50:39 +0300

    Starting create pin component

--- social-media-application/frontend/src/components/CreatePin.jsx
+++ social-media-application/frontend/src/components/CreatePin.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
-const CreatePin = () => {
-  return <div>CreatePin</div>;
+import { client } from "../client";
+import Spinner from "./Spinner";
+import { categories } from "../utils/data";
+
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState("");
+  const [about, setAbout] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [destination, setDestination] = useState();
+  const [fields, setFields] = useState();
+  const [category, setCategory] = useState();
+  const [imageAsset, setImageAsset] = useState();
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col justify-center items-center mt-5 h:h-4/5">
+      CreatePin
+    </div>
+  );
 };
 
 export default CreatePin;
