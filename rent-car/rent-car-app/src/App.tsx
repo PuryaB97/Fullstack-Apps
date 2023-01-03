9dd433c123b2a78fcac40fa32ba2aa315da0034e
Purya Behdarvandan <purya.B97@gmail.com>
2023-01-03 18:47:55 +0300

    HomePage containers

--- rent-car/rent-car-app/src/App.tsx
+++ rent-car/rent-car-app/src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 import "./App.css";
+import { HomePage } from "./app/containers/HomePage";
+
+const AppContainer = styled.div`
+  ${tw`
+  w-full
+  h-full
+  flex
+  flex-col
+`}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="bg-orange-700">Hello world!</h1>
-    </div>
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
   );
 }
 
