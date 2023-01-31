fd984bd7e858e855c02c8b05aacdb8b13699e382
Purya Behdarvandan <purya.B97@gmail.com>
2023-01-31 23:26:34 +0300

    Some fixes

---
+++ rent-car/nestjs-car-app/json-typing.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
--- rent-car/nestjs-car-app/src/app.module.ts
+++ rent-car/nestjs-car-app/src/app.module.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
