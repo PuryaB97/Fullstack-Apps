b18d03224419d4bc02da9e795a15400f892db308
Purya Behdarvandan <purya.B97@gmail.com>
2023-02-06 23:49:20 +0300

    Generating photos from api

--- image-generation/server/routes/dalleRoutes.js
+++ image-generation/server/routes/dalleRoutes.js
@@ -13,7 +13,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-  res.send("Hello from dalle");
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
 router.route("/").post(async (req, res) => {
@@ -24,15 +24,16 @@ router.route("/").post(async (req, res) => {
       prompt,
       n: 1,
       size: "1024x1024",
-      response_foramt: "b46_json",
+      response_format: "b64_json",
     });
 
     const image = aiResponse.data.data[0].b64_json;
-
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error);
-    res.status(500).send(error?.response.data.error.message);
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
   }
 });
 
