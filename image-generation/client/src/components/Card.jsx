c51a2b014bf7fbd43d124741e6165e003bfc307f
Purya Behdarvandan <purya.B97@gmail.com>
2023-02-07 23:09:43 +0300

    Integrate backend to frontend

--- image-generation/client/src/components/Card.jsx
+++ image-generation/client/src/components/Card.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+import { download } from "../assets";
+import { downloadImage } from "../utils";
+
+const Card = ({ _id, name, prompt, photo }) => {
+  return (
+    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
+      <img
+        src={photo}
+        alt="prompt"
+        className="w-full h-auto object-cover rounded-xl"
+      />
+    </div>
+  );
 };
 
 export default Card;
