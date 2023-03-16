fd6b6de41480aa6797cd983cfd17e26e656ced55
Purya Behdarvandan <purya.B97@gmail.com>
2023-03-16 19:56:25 +0300

    Frontend installations and packages

--- image-generation/client/src/pages/CreatePost.jsx
+++ image-generation/client/src/pages/CreatePost.jsx
@@ -48,7 +48,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("http://localhost:8080/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -94,7 +94,7 @@ const CreatePost = () => {
             labelName="Your name"
             type="text"
             name="name"
-            placeholder="John Doe"
+            placeholder="Pouriya Behdarvandan"
             value={form.name}
             handleChange={handleChange}
           />
